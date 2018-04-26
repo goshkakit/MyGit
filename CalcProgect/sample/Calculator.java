a4c5419c3dd48fd661a13b40b68dbf342ffb50bc
Георгий <georgij@MacBook-Air-Georgij.local>
2018-04-26 15:05:16 +0300

    project added

---
+++ CalcProgect/sample/Calculator.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.scene.input.MouseEvent;
+
+public class Calculator {
+    public void add7(MouseEvent mouseEvent) {
+    }
+
+    public void add4(MouseEvent mouseEvent) {
+    }
+
+    public void add1(MouseEvent mouseEvent) {
+    }
+
+    public void add2(MouseEvent mouseEvent) {
+    }
+
+    public void add3(MouseEvent mouseEvent) {
+    }
+
+    public void add5(MouseEvent mouseEvent) {
+    }
+}
